6ff4cca2682bacd02bbef7098b62393d883ae013
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2011-03-10 15:34:52 -0500

    Pull Twitter credentials from ENV by default, for Heroku

--- src/config.rb
+++ src/config.rb
@@ -3,9 +3,9 @@
 $MATCH_TIME = 1800
 
 Twitter.configure do |config|
-    config.consumer_key = $TWITTER_KEY
-    config.consumer_secret = $TWITTER_SECRET
-    config.oauth_token = $TWITTER_TOKEN
-    config.oauth_token_secret = $TWITTER_TOKEN_SECRET
+    config.consumer_key = ENV['TWITTER_KEY']
+    config.consumer_secret = ENV['TWITTER_SECRET']
+    config.oauth_token = ENV['TWITTER_TOKEN']
+    config.oauth_token_secret = ENV['TWITTER_TOKEN_SECRET']
 end
 
